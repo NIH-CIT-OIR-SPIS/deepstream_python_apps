94eeac56bd194a699ff94edc9e5a8e96427c22d8
yeric1789 <76454253+yeric1789@users.noreply.github.com>
2023-08-09 12:34:02 -0400

    Fixed issue with x264enc plugin not linking now with correct caps filter

--- apps/deepstream-test-csi-rtsp/deepstream_test1_csi_out.py
+++ apps/deepstream-test-csi-rtsp/deepstream_test1_csi_out.py
@@ -36,8 +36,7 @@ PGIE_CLASS_ID_VEHICLE = 0
 PGIE_CLASS_ID_BICYCLE = 1
 PGIE_CLASS_ID_PERSON = 2
 PGIE_CLASS_ID_ROADSIGN = 3
-URI_CSI = "argus://"
-URI_USB = "/dev/video"
+
 
 
 def osd_sink_pad_buffer_probe(pad,info,u_data):
